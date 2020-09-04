8ecbb20e9f49dca446647845cf1040d74bbedfeb
olizi <o.zink@zink-projects.de>
2020-09-04 22:29:20 +0200

    added pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-alert('Hello world');
+let pokemonList = [
+  {name: 'Charmander', height: 0.6, type: 'fire'},
+  {name: 'Wigglytuff', height: 0.8, type: ['fairy', 'normal']},
+  {name: 'Snorlax', height: 2.1, type: 'normal'}
+];
 
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+// console.log(pokemonList[1].type[1]);
