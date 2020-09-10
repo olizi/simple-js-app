48e113d54b16d6599348430194732f7be90fe77d
olizi <o.zink@zink-projects.de>
2020-09-10 14:29:35 +0200

    added pokemon lsit output and highlighting large pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
+// list of Pokémon
 let pokemonList = [
   {name: 'Charmander', height: 0.6, type: 'fire'},
   {name: 'Wigglytuff', height: 0.8, type: ['fairy', 'normal']},
   {name: 'Snorlax', height: 2.1, type: 'normal'}
 ];
 
-// console.log(pokemonList[1].type[1]);
+// show list of Pokémon and highlight Pokemon larger 1.5
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height <= 1.5){
+    document.write('<h3>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')</h3>')
+  }else {document.write('<h3>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s big!</h3>')
+  }
+}
