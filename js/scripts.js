04f30b91bc4518322d021a52a11de4c7022e830b
olizi <o.zink@zink-projects.de>
2020-09-16 12:57:09 +0200

    wrap pokemonList array in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,35 @@
 // list of Pokémon
-let pokemonList = [
-  {name: 'Charmander', height: 0.6, type: 'fire'},
-  {name: 'Wigglytuff', height: 0.8, type: ['fairy', 'normal']},
-  {name: 'Snorlax', height: 2.1, type: 'normal'}
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {name: 'Charmander', height: 0.6, type: 'fire'},
+    {name: 'Wigglytuff', height: 0.8, type: ['fairy', 'normal']},
+    {name: 'Snorlax', height: 2.1, type: 'normal'}
+  ];
+
+// add a single item to the pokemonList array
+  function add(pokemon) {
+    if ((typeof pokemon === 'object')&& (typeof pokemon.name === 'string')&& (typeof pokemon.height === 'number')&& (typeof pokemon.type === 'string')) {
+      pokemonList.push(pokemon)
+    }else {alert('data type is not correct')}
+  }
+
+// return all items of pokemonList
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+// adds a new Pokémon to the pokemonList array
+pokemonRepository.add({name: 'Basesam', height: 0.7, type: 'poison'});
 
 // show list of Pokémon and highlight Pokemon larger 1.5
 function printArrayDetails(){
-  pokemonList.forEach(function (pokemon) {
+  pokemonRepository.getAll().forEach(function (pokemon) {
     if (pokemon.height <= 1.5){
       document.write('<h3>' + pokemon.name + ' (height: ' + pokemon.height + ')</h3>')
     }else {document.write('<h3>' + pokemon.name + ' (height: ' + pokemon.height + ') - Wow, that\'s big!</h3>')
